d27fe57047af26f8e511a6969d5b61039c856167
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-03 12:15:38 -0400

    Copy headstart from class

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,38 @@
 (function(){
   'use strict';
 
+  var username = '';
+
   $(document).ready(function(){
-    $('body').prepend(JST['application']());
+
+    route();
+
+    $(document).on('submit', '.login-form', function(event){
+      event.preventDefault();
+      username = $(this).find('.login-form-username').val();
+      window.location.hash = '/chat';
+    });
+
+    $(window).on('hashchange', function(event){
+      event.preventDefault();
+      route();
+    });
   });
+
+  function route() {
+    switch(window.location.hash) {
+      case '':
+        $('.application').html(JST['login']());
+        break;
+      case '#/chat':
+        renderChat();
+        break;
+    }
+  }
+
+  function renderChat() {
+    $('.application').html(JST['chat']());
+    console.log('username:', username);
+  }
+
 })();
