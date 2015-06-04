484099bd41858c0cfce7f4869c512f11cd17a1cc
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-03 21:57:50 -0400

    Display messages on chat screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
 (function(){
   'use strict';
 
-  var username = '';
+  var username = localStorage.getItem('username');
 
   $(document).ready(function(){
 
     route();
-    logListOfMessages();
 
     $(document).on('submit', '.login-form', function(event){
       event.preventDefault();
       username = $(this).find('.login-form-username').val();
+      localStorage.setItem('username', username);
       window.location.hash = '/chat';
     });
 
@@ -26,23 +26,29 @@
         $('.application').html(JST['login']());
         break;
       case '#/chat':
-        renderChat();
+        getMessages();
         break;
     }
   }
 
-  function renderChat() {
-    $('.application').html(JST['chat']());
-    console.log('username:', username);
-  }
-
-  function logListOfMessages() {
+  function getMessages() { //gets list of messages from server
     $.ajax({
       url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/",
-    }).then(function(response) {
-        console.log(response);
-      });
-    };
+    }).then(renderChat);
+  }
+
+
+  function renderChat(data) { //pass messagees to renderChat
+    $('.application').html(JST['chat'](data));
+  }
+
+
+
+
+
+
+
+
 
 
 })();
